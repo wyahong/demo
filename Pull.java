a36c284fbd9021a37026cee489427e9a06e53e72
yahong <wangyahongyx@163.com>
2020-05-03 11:29:42 +0800

    commit for

--- Pull.java
+++ Pull.java
@@ -2,4 +2,10 @@ public class Pull {
     public static void main(String[] args) {
         System.out.println("pull test");
     }
+
+    public void test(){
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i + " :idea");
+        }
+    }
 }
